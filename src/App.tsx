cb00cbca7cf409ac47d6a716f9d6ec9961572370
alireza.azizzadeh <alireza.azizzadeh@gmail.com>
2023-05-25 03:56:32 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReminderList from './components/ReminderList';
+import Reminder from './models/reminder';
+import reminderService from './services/reminder';
+import NewReminder from './components/NewReminder';
+
+// const reminders: Reminder[] =[
+//   {id:1, title:"reminder 1"}
+// ]
+
+
 
 function App() {
+  const [reminders, setReminders]= useState<Reminder[]>([
+    // {id:1, title:"reminder 1"}
+  ]);
+  useEffect(()=>{
+    loadReminders();
+  },[]);
+
+  const loadReminders= async ()=>{
+    const reminders = await reminderService.getReminders();
+    setReminders(reminders);
+  }
+  const removerReminder = (id:number)=>{
+    setReminders(reminders.filter(reminder => reminder.id!==id));
+  }
+
+  const addReminder= async (title:string)=>{
+    const newReminder=await reminderService.addReminder(title);
+    setReminders([newReminder, ...reminders]);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewReminder onAddReminder={addReminder }/>
+      <ReminderList items={reminders} onRemoveReminder={removerReminder}/>
     </div>
   );
 }
